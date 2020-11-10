2e2c6dc19cf276dc394af3edfefd4e2664266c61
April Webb <aprilewebb@gmail.com>
2020-11-09 22:04:14 -0500

    Added header, menu, main components and added CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
